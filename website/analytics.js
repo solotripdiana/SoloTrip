2ac2fa9bc4a0ca499e2a5221a37f49c68baa6b6a
joaomagico <joao.magico@gmail.com>
2025-12-28 23:24:02 +0000

    correct google analytics

--- website/analytics.js
+++ website/analytics.js
@@ -1,11 +1,11 @@
-<!-- Google Analytics (GA4) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-B6HSM62Z1K"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'G-B6HSM62Z1K');
-    page_path: window.location.pathname
-  });
-</script>
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+
+gtag('config', 'G-B6HSM62Z1K', {
+  page_path: window.location.pathname
+});
